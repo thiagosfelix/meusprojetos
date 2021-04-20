5a566717e336a51da402ffce80d8e0819c9aa4f1
Thiago Felix <78563115+thiagosfelix@users.noreply.github.com>
2021-04-20 14:09:31 -0300

    fim app

--- Projeto Aula/javascript-es6-oo/app/app.js
+++ Projeto Aula/javascript-es6-oo/app/app.js
@@ -59,13 +59,60 @@ class Bd {
                 continue;
             }
 
+            despesa.id = i;
             despesas.push(despesa);
         }
         return despesas;
     }
 
     pesquisar(despesa) {
+        let despesasFiltradas = Array();
+        despesasFiltradas = this.recuperarTodosRegistros();
+        
         console.log(despesa);
+        console.log(despesasFiltradas);
+
+        //ano
+        if (despesa.ano != '') {
+            console.log('Filtro de ano');
+            despesasFiltradas = despesasFiltradas.filter(d => d.ano == despesa.ano);
+        };
+
+        //mes
+        if (despesa.mes != '') {
+            console.log('Filtro de mês');
+            despesasFiltradas = despesasFiltradas.filter(d => d.mes == despesa.mes);
+        };
+
+        //dia
+        if (despesa.dia != '') {
+            console.log('Filtro de dia');
+            despesasFiltradas = despesasFiltradas.filter(d => d.dia == despesa.dia);
+        };
+
+        //tipo
+        if (despesa.tipo != '') {
+            console.log('Filtro de tipo');
+            despesasFiltradas = despesasFiltradas.filter(d => d.tipo == despesa.tipo);
+        };
+
+        //descricao
+        if (despesa.descricao != '') {
+            console.log('Filtro de descrição');
+            despesasFiltradas = despesasFiltradas.filter(d => d.descricao == despesa.descricao);
+        };
+
+        //valor
+        if (despesa.valor != '') {
+            console.log('Filtro de valor');
+            despesasFiltradas = despesasFiltradas.filter(d => d.valor == despesa.valor);
+        };
+
+        return despesasFiltradas;
+    }
+
+    remover(id) {
+        localStorage.removeItem(id);
     }
 }
 
@@ -132,16 +179,17 @@ function cadastrarDespesa() {
 
 }
 
-function carregaListaDespesas() {
+function carregaListaDespesas(despesas = Array(), filtro = false) {
 
-    let despesas = Array();
-
-    despesas = bd.recuperarTodosRegistros();
-
+    if (despesas.length == 0 && filtro == false) {
+        despesas = bd.recuperarTodosRegistros();
+    }
+    
     console.log(despesas);
 
     //selecionando o elemento tbody da tabela
     var listaDespesas = document.getElementById('listaDespesas');
+    listaDespesas.innerHTML = '';
 
     //percorrer o array despesas, listando cada despesa de forma dinãmica
     despesas.forEach(function(d) {
@@ -174,6 +222,23 @@ function carregaListaDespesas() {
         linha.insertCell(1).innerHTML = d.tipo;
         linha.insertCell(2).innerHTML = d.descricao;
         linha.insertCell(3).innerHTML = d.valor;
+
+        //criar o botão de exclusão
+        let btn = document.createElement("button");
+        btn.className = 'btn btn-danger';
+        btn.innerHTML = '<i class= "fas fa-times"></i>';
+        btn.id = `id_despesa_${d.id}`
+        btn.onclick = function() {
+            //remover a despesa
+            let id = this.id.replace('id_despesa_', '');
+
+            bd.remover(id);
+
+            window.location.reload();
+        }
+        linha.insertCell(4).append(btn);
+
+        console.log(d);
     })
 }
 
@@ -187,5 +252,8 @@ function pesquisarDespesa() {
 
     let despesa = new Despesa(ano, mes, dia, tipo, descricao, valor);
 
-    bd.pesquisar(despesa);
+    let despesas = bd.pesquisar(despesa);
+
+    this.carregaListaDespesas(despesas, true)
+    
 }
